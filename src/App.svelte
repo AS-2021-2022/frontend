6adf2899f573b919963b688b50b695a8b5f7536e
danielsilva12UA <104782036+danielsilva12UA@users.noreply.github.com>
2022-06-05 16:24:55 +0100

    Some corrections

--- src/App.svelte
+++ src/App.svelte
@@ -9,8 +9,8 @@
 </script>
 
 <script>
-	import Application from './componentes/Application.svelte';
-	import Login from './componentes/Login.svelte'
+	import Application from './components/Application.svelte';
+	import Login from './components/Login.svelte'
 
 	let auth;
 	authenticated.subscribe(a => {auth = a})
@@ -21,7 +21,6 @@
 
 <svelte:head>
 	<title>NSN</title>
-	<link rel='icon' type='image/png' href='https://play-lh.googleusercontent.com/8ddL1kuoNUB5vUvgDVjYY3_6HwQcrg1K2fd_R8soD-e2QYj8fT9cfhfh3G0hnSruLKec'>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
