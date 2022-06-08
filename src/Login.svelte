09884f15e60a4443ef6e29fb45a59ace65f06098
danielsilva12UA <104782036+danielsilva12UA@users.noreply.github.com>
2022-06-09 00:03:56 +0100

    Added error message on failed login

--- src/Login.svelte
+++ src/Login.svelte
@@ -26,6 +26,7 @@ async function login () {
 	if(result["status"] == "rejected")
 	{
 		console.log("unable to login");
+		document.getElementById('failed').classList.add('show')
 	}
 }
 
@@ -40,7 +41,10 @@ async function login () {
           <input type="text" class="form-control" id="user" placeholder="Email Address" required="" autofocus="" />
           <input type="password" class="form-control" id="pass" placeholder="Password" required=""/>      
 
-          <button class="btn btn-lg btn-primary btn-block" on:click={()=>{login();}}>Login</button>   
+          <button class="btn btn-lg btn-primary btn-block mb-3" on:click={()=>{login();}}>Login</button>
+		  <div id="failed" class="alert alert-danger fade" role="alert">
+			Could not authenticate. Please try again.
+		</div>
 		</div>
       </div>
 </main>
