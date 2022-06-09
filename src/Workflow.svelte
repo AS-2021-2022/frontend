ee402df7bcb5db973001477c4672fc3fae0d8169
Tiago <tiagoa3203@gmail.com>
2022-06-09 19:52:07 +0100

    added workflow creation condition

--- src/Workflow.svelte
+++ src/Workflow.svelte
@@ -3,7 +3,7 @@
 
     export let workflowid;
     var last_id = -1;
-
+    let state = "";
     const popovers = []
 
     function showPopover(index) {
@@ -11,10 +11,10 @@
             popovers[index].show()
         }
         catch {
-            const exampleEl = document.getElementById('popover'+index)
-            const popover = new bootstrap.Popover(exampleEl)
-            popovers[index] = popover
-            popover.show()
+            const exampleEl = document.getElementById('popover'+index);
+            const popover = new bootstrap.Popover(exampleEl);
+            popovers[index] = popover;
+            popover.show();
         }
     }
 
@@ -24,6 +24,10 @@
     if(last_id != workflowid)
     {
         last_id = workflowid;
+        if(workflowid == undefined) state = "write";
+        else{
+            state = "read";
+        }
     }
 
     });
@@ -42,43 +46,48 @@
 
 
 <main>
-    Workflow {workflowid}
-    <div class="container-fluid" style="height: 11.5vh;  overflow-x: auto; overflow-y: hidden">
+    {#if state == "read"}
+        <div class="container-fluid" style="height: 11.5vh;  overflow-x: auto; overflow-y: hidden">
 
-    <div class = "box">
+        <div class = "box">
 
-        {#each apiResponse["steps"] as step , index}
-        <div style="display: inline-block">
-            {#if index < apiResponse["progress"]}
-            
-            <a tabindex="0" id="popover{index}" style="border-radius: 50%;" class="btn btn-lg btn-success" role="button" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-trigger="focus" title="Assignee: {step.assignee}" on:click={() => showPopover(index)} data-bs-content="{step.description}">{index}</a>
-            {:else}
-            
-            
-            <a tabindex="0" id="popover{index}" style="border-radius: 50%;" class="btn btn-lg btn-danger" role="button" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-trigger="focus" title="Assignee: {step.assignee}" on:click={() => showPopover(index)} data-bs-content="{step.description}">{index}</a>{/if}
-        </div>
-        {#if index<(apiResponse["steps"].length-1)}
-        <div style="display: inline-block"><h1><i class="bi bi-arrow-right"></i></h1></div>
-        {/if}
-            
-        {/each}
-        </div>
-    </div>
-        <br><br><br>
-        <div class="shadow-lg p-3 mb-5 bg-body rounded">
-        <div class="container-fluid overflow-auto" style="height: 55vh">
-            {#each apiResponse["files"] as file, index}
-            <div class="card mr-3 mb-3" style="width: justify-content; display:inline-block">
-                <img src="https://icons.getbootstrap.com/assets/icons/file-earmark-text-fill.svg" class="card-img-top">
-                <div class="card-body">
-                    {file}
-                    <p></p>
-                    <a href="#" class="btn btn-primary"><i class="bi bi-download"></i></a>
-                </div>
+            {#each apiResponse["steps"] as step , index}
+            <div style="display: inline-block">
+                {#if index < apiResponse["progress"]}
+                
+                <a tabindex="0" id="popover{index}" style="border-radius: 50%;" class="btn btn-lg btn-success" role="button" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-trigger="focus" title="Assignee: {step.assignee}" on:click={() => showPopover(index)} data-bs-content="{step.description}">{index}</a>
+                {:else}
+                
+                
+                <a tabindex="0" id="popover{index}" style="border-radius: 50%;" class="btn btn-lg btn-danger" role="button" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-trigger="focus" title="Assignee: {step.assignee}" on:click={() => showPopover(index)} data-bs-content="{step.description}">{index}</a>{/if}
             </div>
+            {#if index<(apiResponse["steps"].length-1)}
+            <div style="display: inline-block"><h1><i class="bi bi-arrow-right"></i></h1></div>
+            {/if}
+                
             {/each}
             </div>
         </div>
+            <br><br><br>
+            <div class="shadow-lg p-3 mb-5 bg-body rounded">
+            <div class="container-fluid overflow-auto" style="height: 55vh">
+                {#each apiResponse["files"] as file, index}
+                <div class="card mr-3 mb-3" style="width: justify-content; display:inline-block">
+                    <img src="https://icons.getbootstrap.com/assets/icons/file-earmark-text-fill.svg" class="card-img-top">
+                    <div class="card-body">
+                        {file}
+                        <p></p>
+                        <a href="#" class="btn btn-primary"><i class="bi bi-download"></i></a>
+                    </div>
+                </div>
+                {/each}
+                </div>
+            </div>
+    {:else}
+
+            
+
+    {/if}
 </main>
 
 
