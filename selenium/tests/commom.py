e3fee23d448e7d8a9f14f8de625237e8fd04a772
Tiago <tiagoa3203@gmail.com>
2022-06-19 16:18:28 +0100

    updated gitignore / changed tests commom.py

--- selenium/tests/commom.py
+++ selenium/tests/commom.py
@@ -1,5 +1,5 @@
-URL_FRONTEND = "http://localhost:8080"
-URL_BACKEND = "https://tranquil-brook-75958.herokuapp.com/"
+URL_FRONTEND = "https://as-web-app-frontend.herokuapp.com/"
+URL_BACKEND  = "https://tranquil-brook-75958.herokuapp.com/"
 
 
 TOKEN = 1
